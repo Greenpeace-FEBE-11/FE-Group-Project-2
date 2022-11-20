fcdcf587046a6263ca060e03c76ca45ff6a4b398
VaniaSasikirana <sk080501@gmail.com>
2022-11-20 20:05:09 +0700

    try fixed error

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import {BrowserRouter} from "react-router-dom"
 
 
